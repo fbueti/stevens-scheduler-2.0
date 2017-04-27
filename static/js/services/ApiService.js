a0313417d030cf46d2e80fc4a9ba8dcca6f28129
austin <austin.cawley@gmail.com>
2017-04-27 18:02:28 -0400

    structure for CourseService
    Signed-off-by: austin <austin.cawley@gmail.com>

---
+++ static/js/services/ApiService.js
@@ -0,0 +1,6 @@
+/**
+ * Created by austin on 4/27/17.
+ *
+ *
+ * Handles Http Requests to our local api
+ */
---
+++ static/js/services/CourseService.js
@@ -0,0 +1,32 @@
+/**
+ * Created by austin on 4/27/17.
+ *
+ *
+ * Handles HTTP Request to Stevens
+ */
+
+const STEVENS_BASE_URL = 'https://web.stevens.edu/scheduler/core/core.php?cmd=';
+const STEVENS_TERMS_URL = `${STEVENS_BASE_URL}terms`;
+const STEVENS_SEMESTER_URL = `${STEVENS_BASE_URL_URL}getxml&term=`;
+
+class CourseService {
+  static getTerms() {
+    return Vue.http.get(STEVENS_TERMS_URL)
+        .then((response) => {
+          // Todo: Parse XML into list of terms
+        })
+        .catch((error) => {
+          // Todo: Handle?
+        });
+  }
+
+  static getSemester(term) {
+    return Vue.http.get(`${STEVENS_SEMESTER_URL}${term}`)
+        .then((response) => {
+          // Todo: Parse XML into a Semester object
+        })
+        .catch((error) => {
+          // Todo: Handle?
+        });
+  }
+}
