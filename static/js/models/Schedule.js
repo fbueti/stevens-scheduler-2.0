62d564cd340293c2bc0fe49d9279e5c3214f0442
austin <austin.cawley@gmail.com>
2017-04-27 18:27:31 -0400

    Outlined ApiService and Schedule model
    Signed-off-by: austin <austin.cawley@gmail.com>

---
+++ static/js/models/Schedule.js
@@ -0,0 +1,13 @@
+/**
+ * Created by austin on 4/27/17.
+ */
+
+class Schedule {
+  constructor(data) {
+    this.id = data.id;
+    this.courseCodes = data.courses;
+    this.term = data.term;
+    this.name = data.name;
+    this.notes = data.notes;
+  }
+}
--- static/js/services/ApiService.js
+++ static/js/services/ApiService.js
@@ -4,3 +4,71 @@
  *
  * Handles Http Requests to our local api
  */
+const BASE_URL = 'http://localhost:3000/api';
+const SCHEDULES_BASE_URL = `${BASE_URL}/schedules`;
+const USERS_BASE_URL = `${BASE_URL}/users`;
+
+class ApiService {
+
+  // Schedules
+  static getSchedules() {
+    return Vue.http.get(SCHEDULES_BASE_URL)
+        .then((response) => {
+          // Todo: build Schedules
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+
+  static getScheduleById(id) {
+    return Vue.http.get(`${SCHEDULES_BASE_URL}/${id}`)
+        .then((response) => {
+          // Todo: build Schedules
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+
+  static deleteSchedule(schedule) {
+    return Vue.http.delete(`${SCHEDULES_BASE_URL}/${schedule.id}`)
+        .then((response) => {
+          // Todo: build Schedules
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+
+  static updateSchedule(schedule) {
+    return Vue.http.put(`${SCHEDULES_BASE_URL}/${schedule.id}`)
+        .then((response) => {
+          // Todo: build Schedules
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+
+  static createNewSchedule() {
+    return Vue.http.post(SCHEDULES_BASE_URL)
+        .then((response) => {
+          // Success
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+
+  // Users
+  static deleteUserById(id) {
+    return Vue.http.delete(`${USERS_BASE_URL}/${id}`)
+        .then((response) => {
+          // Successful deletion
+        })
+        .catch((error) => {
+          // Todo: Handle error
+        })
+  }
+}
