0ef9c73dc09b56e73b1ef58a1816a230ee1d5f95
Gregory Johnson <gsoccer111@gmail.com>
2017-05-01 15:57:34 -0400

    schedule-preview
    skeleton for schedule-preview component

---
+++ static/js/components/schedule-preview.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Gregory on 5/1/17.
+ */
+import Vue from '../VueSetup';
+import styles from '../../scss/components/schedule-preview.scss';
+
+Vue.component('schedule-preview', {
+    template: '<div class="component-test">' +
+        '<p>This is a styled test Component!</p>' +
+        '</div>',
+});
