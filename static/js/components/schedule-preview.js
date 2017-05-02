068045cbd8842900eedbf51f54ceb91f0eb71829
austin <austin.cawley@gmail.com>
2017-05-02 18:47:51 -0400

    Fixed a bunch of issues in the deletion / updating of schedules.
    Signed-off-by: austin <austin.cawley@gmail.com>

--- static/js/components/schedule-preview.js
+++ static/js/components/schedule-preview.js
@@ -2,10 +2,21 @@
  * Created by Gregory on 5/1/17.
  */
 import Vue from '../VueSetup';
+import Schedule from '../models/Schedule';
+
 import '../../scss/components/schedule-preview.scss';
 
 Vue.component('schedule-preview', {
-    template: '<div class="component-test">' +
-        '<p>This is a styled test Component!</p>' +
-        '</div>',
+  props: {
+    schedule: {
+      type: Schedule,
+      required: true,
+    },
+  },
+  template: '<div class="component-schedule-preview">' +
+  '<h2>{{schedule.name}}</h2>' +
+  '<p>{{schedule.notes}}</p>' +
+  '<a v-bind:href="`/schedule/${schedule.id}`">Edit</a>' +
+  // '<a v-bind:href="/schedules/schedule.id">Delete</a>' +
+  '</div>',
 });
