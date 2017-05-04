9ac65c18c44c509e40fac3bc83c95a6b1c2076b4
Gregory Johnson <gsoccer111@gmail.com>
2017-05-04 17:44:21 -0400

    function calls for course
    function calls created for adding and removing courses

--- static/js/edit.js
+++ static/js/edit.js
@@ -9,38 +9,38 @@ import './components/schedule';
 
 // Styles
 import '../scss/edit.scss';
- 
+
 const app = new Vue({
-  el: '#app',
-  data: {
-    id: () => {
-		var url = window.location.href;
-		for(var i = url.length - 1; i >= 0; i--) {
-			if(url.charAt(i) === '/') {
-				return url.substr(i);
-			}
-		}
-	},
-  },
-  asyncComputed: {
-    schedule: {
-      async get() {
-         return ApiService.getScheduleById(this.id);
-      },
-      default() {
-         return null;
-      },
-   },
-  },
-  methods: {
-     addCourse() {
+    el: '#app',
+    data: {
+        id: () => {
+            var url = window.location.href;
+            for (var i = url.length - 1; i >= 0; i--) {
+                if (url.charAt(i) === '/') {
+                    return url.substr(i);
+                }
+            }
+        },
+    },
+    asyncComputed: {
+        schedule: {
+            async get() {
+                    return ApiService.getScheduleById(this.id);
+                },
+                default () {
+                    return null;
+                },
+        },
+    },
+    methods: {
+        addCourse(course) {
 
-     },
-     removeCourse() {
+            },
+            removeCourse(course) {
 
-     },
-     saveCourse() {
+            },
+            saveSchedule(course) {
 
-     },
-  },
+            },
+    },
 });
