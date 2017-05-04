bb8f02c74bd03da9d7db63e62f5c360d298dea0c
austin <austin.cawley@gmail.com>
2017-05-04 18:07:25 -0400

    small model fixes
    Signed-off-by: austin <austin.cawley@gmail.com>

--- static/js/models/Schedule.js
+++ static/js/models/Schedule.js
@@ -5,31 +5,27 @@ import Vue from '../VueSetup';
 import Term from './Term';
 
 class Schedule {
-    constructor(data) {
-        this.id = data._id || data.id;
-        this.courseCodes = data.courses;
-        this.termCode = data.termCode;
-        this.name = data.name;
-        this.notes = data.notes;
-    }
+  constructor(data) {
+    this.id = data._id || data.id;
+    this.courseCodes = data.courses;
+    this.termCode = data.termCode;
+    this.name = data.name;
+    this.notes = data.notes;
+  }
 
-	function addCourse(course) {
-		this.courseCodes.push(course.callNumber);
-	}
-	
-	function removeCourse(course) {
-		this.courseCodes.splice(this.courseCodes.indexOf(course.callNumber), 1);
-	}
-	
-	function saveSchedule(course) {
-		
-	}
+  addCourse(course) {
+    this.courseCodes.push(course.callNumber);
+  }
 
-    static makeEmpty() {
-        return new Schedule({
-            id: ''
-        });
-    }
+  removeCourse(course) {
+    this.courseCodes.splice(this.courseCodes.indexOf(course.callNumber), 1);
+  }
+
+  static makeEmpty() {
+    return new Schedule({
+      id: '',
+    });
+  }
 }
 
 export default Schedule;
